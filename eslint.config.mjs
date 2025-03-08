ca4c177fa6a79af87a0d64411cbf8bad6268c7a2
zugdev <zug.dev@proton.me>
2025-03-08 03:20:43 -0300

    feat: cli v1

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -56,6 +56,7 @@ export default tsEslint.config({
     "sonarjs/no-element-overwrite": "error",
     "sonarjs/no-identical-conditions": "error",
     "sonarjs/no-identical-expressions": "error",
+    "sonarjs/prefer-nullish-coalescing": "off",
     "@typescript-eslint/naming-convention": [
       "error",
       {
